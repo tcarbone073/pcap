4eee3ecdc6a31d51d178a6b33cf7dc2a89cf4293
Tyler Carbone <tcarbone@LinuxBox.home>
2022-01-06 17:21:06 -0500

    Ready. Set. Go!

---
+++ lecture01.py
@@ -0,0 +1,27 @@
+"""
+Variables
+Assignment Operator
+Strings
+Integer Datatype
+"""
+
+print('Hello, World')
+
+number = 8852
+
+print(number)
+
+weight = 22
+
+answer = number + weight
+
+print(answer)
+
+firstname = 'Tyler'
+lastname = 'Carbone'
+fullname = firstname + lastname
+
+# Conversion from int to str
+sentence = firstname + ' ' + lastname + ' weighs ' + str(weight) + ' lbs.'
+print(sentence)
+
