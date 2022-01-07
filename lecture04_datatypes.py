fab41b4cf41a7e2b902c8a78054ba1023d8ac8ed
Tyler Carbone <tcarbone@LinuxBox.home>
2022-01-07 07:23:02 -0500

    Add lectures 4 and 6

---
+++ lecture04_datatypes.py
@@ -0,0 +1,9 @@
+"""
+Types
+
+print(type(12))     # int
+print(type(12.08))  # float
+print(type("12"))   # str
+print(type(True))   # bool (only True or False)
+
+
---
+++ lecture06_arithmetic.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+
+num1 = 3
+num2 = 10
+
+ans = num1/num2
+print(ans)
+
+ans = num1-num2
+print(ans)
+
+ans = num1%num2 # modulus
+print(ans)
+
+ans = 10 + 3 * 9 - 4 # basic order of operations is followed
+print(ans)
