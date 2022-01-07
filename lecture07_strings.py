346444376976341543470e2179b337fb9908b375
Tyler Carbone <tcarbone@LinuxBox.home>
2022-01-07 07:56:46 -0500

    Add lecture 7

---
+++ lecture07_strings.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+
+sentece = 'This is a sentence.'
+print(sentece)
+
+sentence = "I'm coming home."
+print(sentence)
+
+sentence = "This sentence is\nsplit across lines."
+print(sentence)
+print(sentece[0])
+print(sentece[-1])
+print(sentece[0:-2])
+print(sentece[-2:0])
+
+seq = 'abcdefg'
+print(seq[0:7])
+print(seq[:7:2])
