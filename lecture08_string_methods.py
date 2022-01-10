231958c5b3aa24901a6a473962682fec502196eb
Tyler Carbone <tcarbone@LinuxBox.home>
2022-01-10 05:54:42 -0500

    Add lectures 8 and 9

---
+++ lecture08_string_methods.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+sentence = 'this is a sentence.'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+
+print(sentence.isdigit())
+print(sentence.islower())
+
+print(sentence.isalnum())
+
+print(sentence.startswith('this'))
+print(sentence.startswith('is', 5))
+
+print(sentence.startswith('This'))
+print(sentence.endswith("."))
---
+++ lecture09_string_formatting.py
@@ -0,0 +1,8 @@
+"""
+
+"""
+sentence = 'The sum of {0} + {1} is {2}'.format(0, 1, 2)
+sentence = 'The sum of {0} + {2} is {1}'.format(10, 15, 20)
+print(sentence)
+
+
